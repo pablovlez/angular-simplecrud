5487801d278922b3632ee5036d4690f64438d1a8
Pablo Velez <pablo.vlez@gmail.com>
2019-08-11 16:33:42 -0500

    angular simple crud

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { Employee } from './models/employee';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-simplecrud';
-}
+  employeeArray: Employee[] = [
+    {id: 1, name: "Ryan", country: "USA"},
+    {id: 2, name: "Angelica", country: "USA"},
+    {id: 3, name: "Joe", country: "USA"},
+  ];
+
+  selectedEmployee: Employee = new Employee();
+
+  addOrEdit(){
+    if(this.selectedEmployee.id === 0){
+      this.selectedEmployee.id = this.employeeArray.length + 1;
+      this.employeeArray.push(this.selectedEmployee);
+    }
+    
+    this.selectedEmployee = new Employee();    
+  }
+
+  openForEdit(employee: Employee){
+    this.selectedEmployee = employee;
+  }
+
+  delete(){
+    if(confirm('Are you sure you want to delete it?')){
+      this.employeeArray = this.employeeArray.filter(x  => x != this.selectedEmployee);
+      this.selectedEmployee = new Employee();
+    }     
+  }
+} 
+ 
